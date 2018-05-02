4d88d47c514f6dd0c02eeda581e9d38a5f88046c
lhl <271072547@coolpad.com>
2018-05-02 18:01:06 +0800

    refactor

--- src/index.js
+++ src/index.js
@@ -1,62 +1,6 @@
 // src/index.js
-
-import { createStore } from "redux";
-import { combineReducers } from 'redux';
-
-const ADD_TO_CART = 'ADD_TO_CART';
-
-const initialState = {
-  cart: [
-    {
-      product: 'bread 700g',
-      quantity: 2,
-      unitCost: 90
-    },
-    {
-      product: 'milk 500ml',
-      quantity: 1,
-      unitCost: 47
-    }
-  ]
-}
-
-const productsReducer = function(state=[], action) {
-  return state;
-}
-
-const cartReducer = function(state=initialState, action) {
-  switch (action.type) {
-    case ADD_TO_CART: {
-      return {
-        ...state,
-        cart: [...state.cart, action.payload]
-      }
-    }
-
-    default:
-      return state;
-  }
-}
-
-function addToCart(product, quantity, unitCost) {
-  return {
-    type: ADD_TO_CART,
-    payload: {
-      product,
-      quantity,
-      unitCost
-    }
-  }
-}
-
-const allReducers = {
-  products: productsReducer,
-  shoppingCart: cartReducer
-}
-
-const rootReducer = combineReducers(allReducers);
-
-let store = createStore(rootReducer);
+import store from './store.js';
+import { addToCart }  from './actions/cart-actions';
 
 console.log("initial state: ", store.getState());
 
@@ -67,4 +11,3 @@ store.subscribe(() =>
 store.dispatch(addToCart('Coffee 500gm', 1, 250));
 store.dispatch(addToCart('Flour 1kg', 2, 110));
 store.dispatch(addToCart('Juice 2L', 1, 250));
-

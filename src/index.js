76bab530142900a43eb07127cd0b74268235f750
lhl <271072547@coolpad.com>
2018-05-02 17:09:07 +0800

    initial

---
+++ src/index.js
@@ -0,0 +1,21 @@
+// src/index.js
+
+import { createStore } from "redux";
+import { combineReducers } from 'redux';
+
+const productsReducer = function(state=[], action) {
+  return state;
+}
+
+const cartReducer = function(state=[], action) {
+  return state;
+}
+
+const allReducers = {
+  products: productsReducer,
+  shoppingCart: cartReducer
+}
+
+const rootReducer = combineReducers(allReducers);
+
+let store = createStore(rootReducer);
